8dffe7e5dec7365decab5b4c355d328d2232cfc2
DZLprog <120525349+DZLprog@users.noreply.github.com>
2023-03-26 22:12:38 +0100

    Update TimeFernet_Gui.py

--- tp-pyrochat_student/source/TimeFernet_Gui.py
+++ tp-pyrochat_student/source/TimeFernet_Gui.py
@@ -11,28 +11,22 @@ class TimeFernetGui(fernetGui):
         super().__init__()
 
     def encrypt(self, plaintext):
-        """Encrypts the plaintext message with a Fernet key.
+        #Encrypts the plaintext message with a Fernet key.
 
-        Args:
-            plaintext (str): The message to encrypt.
+        #Args: plaintext (str): The message to encrypt.
 
-        Returns:
-            bytes: The encrypted message.
-        """
+        #Returns: bytes: The encrypted message.
         return self._fernet.encrypt(bytes(plaintext, "utf8"))
 
     def decrypt(self, data):
-        """Decrypts a message with a Fernet key and verifies its time-to-live.
+        #Decrypts a message with a Fernet key and verifies its time-to-live.
 
-        Args:
-            data (bytes): The encrypted message.
+        #Args:data (bytes): The encrypted message.
 
-        Returns:
-            str: The decrypted plaintext message.
+        #Returns: str: The decrypted plaintext message.
 
-        Raises:
-            InvalidToken: If the message cannot be decrypted due to an invalid Fernet key or an expired TTL.
-        """
+        #Raises:InvalidToken: If the message cannot be decrypted due to an invalid Fernet key or an expired TTL.
+        
         encrypted_data = b64decode(data)    # Decode the base64-encoded message.
         current_time = int(time.time())     # Get the current time to check the TTL.
 
